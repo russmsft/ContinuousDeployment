9f496b7c3a393b8f63bed95f4305810dc763b4b1
rubradfo <rubradfo@microsoft.com>
2018-03-20 12:59:46 +0000

    Commit all

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page Russell.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page Russell.";
 
             return View();
         }
